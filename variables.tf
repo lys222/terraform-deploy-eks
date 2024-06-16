256fa6b7a974804de1c5da9d68e9fb63586f3e7a
lys222 <youngseo222@gmail.com>
2024-06-16 13:23:43 +0900

    edit variables.tf

--- variables.tf
+++ variables.tf
@@ -5,71 +5,71 @@ variable "region" {
 variable "cluster_name" {
     description = "Cluster Name"
     type = string
-    default = null
+    default = "test"
 }
 
 variable "cluster_version"{
     description = "Cluster Version"
     type = string
-    default = null
+    default = "1.28"
 }
 
 variable "vpc_cidr" {
     description = "VPC CIDR Range"
     type = string
-    default = null
+    default = "10.0.0.0/16"
 }
 
 variable "ami_type" {
     description = "Node AMI type"
     type = string
-    default = null
+    default = "AL2_x86_64"
 }
 
 variable "instance_type" {
     description = "Node Instance type"
     type = list(string)
-    default = null
+    default = ["t2.micro"]
 }
 
 variable "disk_size" {
     description = "Node Disk Size"
     type = number
-    default = null
+    default = 50
 }
 
 variable "min_size" {
     description = "Node Min Size"
     type = number
-    default = null
+    default = 1
 }
 
 variable "max_size" {
     description = "Node Max Size"
     type = number
-    default = null
+    default = 1
 }
 
 variable "desired_size" {
     description = "Node Desired Size"
     type = number
-    default = null
+    default = 1
 }
 
 variable "iam_arn" {
     description = "IAM Role ARN for EKS"
     type = string
-    default = null
+    default = ${{ vars.IAM_ROLE_ARN }}
 }
 
 variable "policy_arn" {
     description = "IAM Policy for EKS"
     type = string
-    default = null
+    default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 }
 
 variable "ec2_ssh_key_name" {
     description = "EC2 SSH Key name"
     type = string
-    default = null
+    default = ysKey
 }
